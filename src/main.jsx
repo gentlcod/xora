36649474be6e2d63b23488e649cefe454be9876b
Abdul Muhaymin <abood.olabi2002@gmail.com>
2025-01-04 14:04:02 +0300

    fixed issues

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+/* eslint-disable react/no-deprecated */
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom'; // Use ReactDOM for React 17
+import './index.css';
+import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+  document.getElementById('root') // Use render method
+);
